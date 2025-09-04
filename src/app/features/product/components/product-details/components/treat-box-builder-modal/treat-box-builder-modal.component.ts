450d6969082e3c5704c386e386cd61b1239e71d1
Gabriel Zanguettin <gabriel.zanguettin@ciag.org.br>
2025-09-03 21:49:52 -0300

    feat: enhance treat box builder modal with improved layout and functionality

--- src/app/features/product/components/product-details/components/treat-box-builder-modal/treat-box-builder-modal.component.ts
+++ src/app/features/product/components/product-details/components/treat-box-builder-modal/treat-box-builder-modal.component.ts
@@ -60,33 +60,6 @@ export class TreatBoxBuilderModalComponent {
     return index;
   }
 
-  toggleDropdown(): void {
-    this.dropdownOpen = !this.dropdownOpen;
-  }
-
-  closeDropdown(): void {
-    setTimeout(() => (this.dropdownOpen = false), 150);
-  }
-
-  removeSelectedProduct(product: Product, event: MouseEvent): void {
-    event.stopPropagation();
-    this.selectedProducts = this.selectedProducts.filter(p => p.id !== product.id);
-  }
-
-  toggleSelectProduct(product: Product, event: MouseEvent): void {
-    event.stopPropagation();
-    const index = this.selectedProducts.findIndex(p => p.id === product.id);
-    if (index !== -1) {
-      this.selectedProducts.splice(index, 1);
-    } else {
-      this.selectedProducts.push(product);
-    }
-  }
-
-  isProductSelected(product: Product): boolean {
-    return this.selectedProducts.some(p => p.id === product.id);
-  }
-
   adicionarAoCarrinho() {
     console.log(`Adicionado ${this.quantity} caixinhas ao carrinho`);
     this.closeModal();
